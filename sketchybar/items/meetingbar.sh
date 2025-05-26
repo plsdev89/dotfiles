b812428793ca048ad043815732b3928579275b6f
plsdev89 <plsdev89@gmail.com>
2025-05-26 09:03:25 -0400

    Disable sketchybar

--- sketchybar/items/meetingbar.sh
+++ sketchybar/items/meetingbar.sh
@@ -2,13 +2,14 @@
 
 COLOR="$MAGENTA"
 
-sketchybar --add alias "MeetingBar,Item-0" e \
+sketchybar --add alias "MeetingBar,Item-0" right \
 	--rename "MeetingBar,Item-0" meetingbar_alias \
 	--set meetingbar_alias \
 	update_freq=180 \
 	alias.color="0xffF0F1F1" \
 	icon="" \
 	label.drawing=off \
+  background.padding_left=15  \
 	icon.color="0xffF0F1F1" \
 	icon.padding_left=10 \
 	icon.padding_right=3 \
